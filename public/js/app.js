395fd934a9df13a0c8e3459e5e2919f654af7cd4
Dan Neumann <danrodneu@gmail.com>
2015-11-18 19:40:45 -0600

    Generalize input char counter

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,10 @@
    javascript but before any template-specific js.
  */
 
+////////////////////////////////////////////////////////////
+// $.timeago
+////////////////////////////////////////////////////////////
+
 // Configure timeago and initialize it for any <abbr> tag with the 
 // 'timeago' class.
 
@@ -29,6 +33,12 @@ $.timeago.settings.strings = {
 
 $("abbr.timeago").timeago();
 
+////////////////////////////////////////////////////////////
+// Custom jQuery methods
+////////////////////////////////////////////////////////////
+
+// $.fn.focusEnd()
+//
 // Create jQuery method $('input').focusEnd() that
 // focuses the input but also places cursor at the end of the value.
 //
@@ -51,4 +61,54 @@ $("abbr.timeago").timeago();
     }
     return this;
   }; 
+})(jQuery);
+
+// $.fn.counter()
+//
+// define jQuery .counter() method to make it easy to link up an
+// input with a counter that updates as the user types in input.
+
+(function($) {
+  $.fn.counter = function(counterSelector, min, max) {
+    var $counter = $(counterSelector);
+    var $input = $(this);
+
+    // build counter dom
+    var html = '' +
+      '<span class="__counter-container">' +
+      '  <span class="__counter-value">0</span>/' + max.toString() +
+      '</span>';
+    $counter.html(html);
+
+    // create updater function
+    var updateCounter = makeUpdateCounter($input, $counter, min, max);
+
+    $input.on('change keyup', updateCounter);
+    updateCounter();
+
+    return this;
+  };
+
+  // Helpers
+
+  function makeUpdateCounter($input, $counter, min, max) {
+    // track whether user has touched the input so that we only enter
+    // error state on min-constraint if user has touched the input 
+    // because we don't want to overwhelm user with red errors on page load
+    // just because all the inputs are empty
+    var isDirty = false;
+
+    return function updateCounter() {
+      if ($input.val().length > 0) {
+        isDirty = true;
+      }
+      var count = $input.val().length;
+      $counter.find('.__counter-value').text(count);
+      if (count > max || (count < min && isDirty)) {
+        $counter.find('.__counter-container').css('color', '#e74c3c');
+      } else {
+        $counter.find('.__counter-container').css('color', '#777');
+      }
+    };
+  }
 })(jQuery);
