9000c099b9a0a53d66aad09a18f090a796e6f903
Dan Neumann <danrodneu@gmail.com>
2016-05-26 14:37:17 -0500

    Improve presenters

--- src/presenters.js
+++ src/presenters.js
@@ -1,38 +1,45 @@
 'use strict';
-
 // 3rd party
 const assert = require('better-assert');
 const _ = require('lodash');
 
+// Presents are functions that take data from the database
+// and extend/improve it for the view layer. They're used in routes.
+//
+// For instance, a presenter would promote a string timestamp into
+// a Date object which is more convenient for the view layer to work with.
+//
+// Example:
+//
+//     let user = presentUser(yield db.getUser(42));
+//     this.assert(user, 404);
+//     this.body = user;
+
 ////////////////////////////////////////////////////////////
 
-exports.presentUser = function(u) {
+exports.presentUser = function (x) {
+  if (!x) return;
   // Fix embedded json representation
-  if (_.isString(u.created_at))
-    u.created_at = new Date(u.created_at);
-
-  u.url = `/users/${u.uname}`;
-
-  return u;
-};
-
-////////////////////////////////////////////////////////////
-
-exports.presentSession = function(x) {
-  // Fix embedded json representation
-  if (_.isString(x.created_at))
-    x.created_at = new Date(x.created_at);
-  if (_.isString(x.expired_at))
-    x.expired_at = new Date(x.expired_at);
-
+  if (_.isString(x.created_at)) x.created_at = new Date(x.created_at);
+  x.url = `/users/${x.uname}`;
   return x;
 };
 
 ////////////////////////////////////////////////////////////
 
-exports.presentMessage = function(m) {
-  if (m.user)
-    m.user = exports.presentUser(m.user);
-  m.url = `/messages/${m.id}`;
-  return m;
+exports.presentSession = function (x) {
+  if (!x) return;
+  // Fix embedded json representation
+  if (_.isString(x.created_at)) x.created_at = new Date(x.created_at);
+  if (_.isString(x.expired_at)) x.expired_at = new Date(x.expired_at);
+  return x;
+};
+
+////////////////////////////////////////////////////////////
+
+exports.presentMessage = function (x) {
+  if (!x) return;
+  exports.presentUser(x.user);
+  x.url = `/messages/${x.id}`;
+  return x;
 };
