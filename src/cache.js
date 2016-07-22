8ba9a2ed08e7143dadaffa42e127e061d8d16113
Dan Neumann <danrodneu@gmail.com>
2016-07-22 12:17:06 -0500

    Add cache comment

--- src/cache.js
+++ src/cache.js
@@ -5,6 +5,14 @@ const co = require('co');
 // 1st
 const db = require('./db');
 
+//
+// Some things are too wasteful to calculate on every request,
+// but too trivial to extract into a real caching layer. 
+// e.g. SELECT COUNT(*)
+// For these things, a setInterval on each server gets the job
+// done. 
+//
+
 ////////////////////////////////////////////////////////////
 
 module.exports = new IntervalCache({ throwIfKeyNotFound: true })
