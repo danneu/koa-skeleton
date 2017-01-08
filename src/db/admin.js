db1083f1345ccc811629cce817104eb9ccbc5aa4
Dan Neumann <danrodneu@gmail.com>
2017-01-08 11:53:49 -0600

    Add q to admin query

--- src/db/admin.js
+++ src/db/admin.js
@@ -6,7 +6,7 @@ const {q} = require('pg-extra')
 // only counts visible messages, not hidden ones since they are effectively
 // deleted
 exports.getStats = async function () {
-  return pool.one(`
+  return pool.one(q`
     SELECT
       (SELECT COUNT(*) FROM users) AS users_count,
       (SELECT COUNT(*) FROM messages WHERE is_hidden = false) AS messages_count
