267badf040060103db9c3958b202c13e8912e994
Dan Neumann <danrodneu@gmail.com>
2017-11-20 15:58:15 -0600

    Use connectionString to connect to postgres

--- src/db/util.js
+++ src/db/util.js
@@ -1,11 +1,11 @@
 // 3rd
-const { extend, parseUrl } = require('pg-extra')
+const { extend } = require('pg-extra')
 const pg = extend(require('pg'))
 // 1st
 const config = require('../config')
 
 // This is the connection pool the rest of our db namespace
 // should import and use
-const pool = new pg.Pool(parseUrl(config.DATABASE_URL))
+const pool = new pg.Pool({ connectionString: config.DATABASE_URL })
 
 module.exports = { pool }
