8c2e0e378edb1674baf237aff5ee92be15a5ee2a
Dan Neumann <danrodneu@gmail.com>
2018-04-12 14:52:19 -0500

    Update templating dep to silence key prop warnings

--- src/index.js
+++ src/index.js
@@ -46,7 +46,10 @@ app.use(mw.wrapFlash())
 app.use(bouncer.middleware())
 app.use(mw.handleBouncerValidationError()) // Must come after bouncer.middleware()
 const viewsRoot = require('path').join(__dirname, 'views')
-app.use(mw.reactRender(viewsRoot, { parent: 'master' }))
+app.use(mw.reactRender(viewsRoot, { 
+  parent: 'master',
+  keyPropWarnings: false,
+}))
 
 // Provide a convience function for protecting our routes behind
 // our authorization rules. If authorization check fails, 404 response.
