51e9d696e87ecf45fdb1174471943dfdc5551dfd
Dan Neumann <danrodneu@gmail.com>
2018-08-21 16:49:40 -0500

    Bump pg-extra to 2.0.x

--- src/db/util.js
+++ src/db/util.js
@@ -6,8 +6,8 @@ const { DATABASE_URL } = require('../config')
 
 // This is the connection pool singleton the rest of our db namespace
 // should import and use
-const pool = new pg.Pool({ connectionString: DATABASE_URL })
+const pool = new pg.extra.Pool({ connectionString: DATABASE_URL })
 
-const getClient = () => new pg.Client({ connectionString: DATABASE_URL })
+const getClient = () => new pg.extra.Client({ connectionString: DATABASE_URL })
 
 module.exports = { pool, getClient }
